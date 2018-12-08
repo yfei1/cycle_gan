4b87de17cd80defebf8cb9901daf353ef5d07eb1
ruolantang <ruolantang007@gmail.com>
2018-12-08 18:11:26 -0500

    model debug

--- model.py
+++ model.py
@@ -1,34 +1,33 @@
 import tensorflow as tf
-import numpy as 
+import numpy as np
 import os
 from scipy.misc import imsave
 import tensorflow.contrib.gan as gan
 
-# from __future__ import division
-# import time
-# from glob import glob
-# from collections import namedtuple
-
 from module import *
-from utils import *
+# from utils import *
 
 
 BATCH_SIZE = 1
 INPUT_WIDTH = 256
 INPUT_DIM = 3
-X_PATH = './datasets/monet2photo/trainA'
-Y_PATH = './datasets/monet2photo/trainB'
+X_PATH = './datasets/apple2orange/trainA'
+Y_PATH = './datasets/apple2orange/trainB'
 MODE = 'train'
 OUT = './output'
 log_every = 10
 save_every = 10
 L1_lambda = 10
 RESTORE = False
+epochs = 2
+learn_rate = 2e-4
+beta1 = 0.5
+MODE = 'train'
 
 class Model(object):
     def __init__(self):
         self.discriminator = discriminator
-        self.generator = generator
+        self.generator = generator_resnet
         # self.criterionGAN = mae_criterion
 
         self.X = tf.placeholder(tf.float32, [None, INPUT_WIDTH, INPUT_WIDTH, INPUT_DIM])
@@ -36,72 +35,73 @@ class Model(object):
         self.X2Y_sample = tf.placeholder(tf.float32, [None, INPUT_WIDTH, INPUT_WIDTH, INPUT_DIM])
         self.Y2X_sample = tf.placeholder(tf.float32, [None, INPUT_WIDTH, INPUT_WIDTH, INPUT_DIM])
 
-        self.X2Y = self.generator(self.X, self.options, False, name="generatorX2Y")
-        self.X2Y2X = self.generator(self.X2Y, self.options, False, name="generatorY2X")
-        self.Y2X = self.generator(self.Y, self.options, True, name="generatorY2X")
-        self.Y2X2Y = self.generator(self.Y2X, self.options, True, name="generatorX2Y")
+        self.X2Y = self.generator(self.X, self.Y, False, name="generatorX2Y")
+        self.X2Y2X = self.generator(self.X2Y, self.X, False, name="generatorY2X")
+        self.Y2X = self.generator(self.Y, self.X, True, name="generatorY2X")
+        self.Y2X2Y = self.generator(self.Y2X, self.Y, True, name="generatorX2Y")
 
-        with tf.variable_scope('discriminatorY'):
-            self.d_X2Y = self.discriminator(self.X2Y, self.options, reuse=False)
-        with tf.variable_scope('discriminatorX'):
-            self.d_Y2X = self.discriminator(self.Y2X, self.options, reuse=False)
+        self.d_X2Y = self.discriminator(self.X2Y, reuse=False, name="discriminatorY")
+        self.d_Y2X = self.discriminator(self.Y2X, reuse=False, name="discriminatorX")
 
-        with tf.variable_scope('discriminatorY'):
-            self.d_Y = self.discriminator(self.Y, self.options, reuse=True)
-        with tf.variable_scope('discriminatorX'):
-            self.d_X = self.discriminator(self.X, self.options, reuse=True)
-        with tf.variable_scope('discriminatorY'):
-            self.d_Y2X_sample = self.discriminator(self.Y2X_sample, self.options, reuse=True)
-        with tf.variable_scope('discriminatorX'):
-            self.d_X2Y_sample = self.discriminator(self.X2Y_sample, self.options, reuse=True)
+        self.d_Y = self.discriminator(self.Y, reuse=True, name="discriminatorY")
+        self.d_X = self.discriminator(self.X, reuse=True, name="discriminatorX")
+        self.d_Y2X_sample = self.discriminator(self.Y2X_sample, reuse=True, name="discriminatorY")
+        self.d_X2Y_sample = self.discriminator(self.X2Y_sample, reuse=True, name="discriminatorX")
 
         # Declare losses, optimizers(trainers) and fid for evaluation
+        t_vars = tf.trainable_variables()
+        self.d_vars = [var for var in t_vars if 'discriminator' in var.name]
+        self.g_vars = [var for var in t_vars if 'generator' in var.name]
         self.g_loss = self.g_loss_function()
         self.d_loss = self.d_loss_function()
         self.g_train = self.g_trainer()
         self.d_train = self.d_trainer()
-        # self.fid = self.fid_function()
+        self.fid = self.fid_function()
 
     def g_loss_function(self):
-        g_loss_X2Y = tf.losses.mean_squared_error(self.d_X2Y, tf.ones_like(self.d_X2Y))
-        g_loss_Y2X = tf.losses.mean_squared_error(self.d_Y2X, tf.ones_like(self.d_Y2X))
-        cyc_loss_X = tf.losses.absolute_difference(self.X, self.X2Y2X)
-        cyc_loss_Y = tf.losses.absolute_difference(self.Y, self.Y2X2Y)
+        g_loss_X2Y = tf.reduce_mean(tf.losses.mean_squared_error(self.d_X2Y, tf.ones_like(self.d_X2Y)))
+        g_loss_Y2X = tf.reduce_mean(tf.losses.mean_squared_error(self.d_Y2X, tf.ones_like(self.d_Y2X)))
+        cyc_loss_X = tf.reduce_mean(tf.losses.absolute_difference(self.X, self.X2Y2X))
+        cyc_loss_Y = tf.reduce_mean(tf.losses.absolute_difference(self.Y, self.Y2X2Y))
         g_loss = g_loss_X2Y + g_loss_Y2X + cyc_loss_X * L1_lambda + cyc_loss_Y * L1_lambda
         return g_loss
 
     def d_loss_function(self):
-        d_Y_loss_real = tf.losses.mean_squared_error(self.d_Y, tf.ones_like(self.d_Y))
-        d_Y_loss_fake = tf.losses.mean_squared_error(self.d_X2Y_sample, tf.zeros_like(self.d_X2Y_sample))
+        d_Y_loss_real = tf.reduce_mean(tf.losses.mean_squared_error(self.d_Y, tf.ones_like(self.d_Y)))
+        d_Y_loss_fake = tf.reduce_mean(tf.losses.mean_squared_error(self.d_X2Y_sample, tf.zeros_like(self.d_X2Y_sample)))
         d_Y_loss = (d_Y_loss_real + d_Y_loss_fake) / 2
-        d_X_loss_real = tf.losses.mean_squared_error(self.d_X, tf.ones_like(self.d_X))
-        d_X_loss_fake = tf.losses.mean_squared_error(self.d_Y2X_sample, tf.zeros_like(self.d_Y2X_sample))
-        d_X_loss = (da_loss_real + da_loss_fake) / 2
+        d_X_loss_real = tf.reduce_mean(tf.losses.mean_squared_error(self.d_X, tf.ones_like(self.d_X)))
+        d_X_loss_fake = tf.reduce_mean(tf.losses.mean_squared_error(self.d_Y2X_sample, tf.zeros_like(self.d_Y2X_sample)))
+        d_X_loss = (d_X_loss_real + d_X_loss_fake) / 2
         d_loss = d_Y_loss + d_X_loss
         return d_loss
 
     def g_trainer(self):
-        g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator')
-        g_solver = tf.train.AdamOptimizer(args.learn_rate, args.beta1).minimize(self.g_loss, var_list=g_vars)
+        g_solver = tf.train.AdamOptimizer(learn_rate, beta1).minimize(self.g_loss, var_list=self.g_vars)
         return g_solver
 
     def d_trainer(self):
-        d_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
-        d_solver = tf.train.AdamOptimizer(args.learn_rate, args.beta1).minimize(self.d_loss, var_list=d_vars)
+        d_solver = tf.train.AdamOptimizer(learn_rate, beta1).minimize(self.d_loss, var_list=self.d_vars)
         return d_solver
 
-    # def fid_function(self):
-    #     INCEPTION_IMAGE_SIZE = (299, 299)
-    #     real_resized = tf.image.resize_images(self.image_batch, INCEPTION_IMAGE_SIZE)
-    #     fake_resized = tf.image.resize_images(self.g_output, INCEPTION_IMAGE_SIZE)
-    #     return gan.eval.frechet_classifier_distance(real_resized, fake_resized, gan.eval.run_inception)
+    def fid_function(self):
+        INCEPTION_IMAGE_SIZE = (299, 299)
+        real_resized1 = tf.image.resize_images(self.X, INCEPTION_IMAGE_SIZE)
+        fake_resized1 = tf.image.resize_images(self.Y2X, INCEPTION_IMAGE_SIZE)
 
-def buildDataset(self, x_path = X_PATH, y_path = Y_PATH):        
+        real_resized2 = tf.image.resize_images(self.Y, INCEPTION_IMAGE_SIZE)
+        fake_resized2 = tf.image.resize_images(self.X2Y, INCEPTION_IMAGE_SIZE)
+
+        d1 = gan.eval.frechet_classifier_distance(real_resized1, fake_resized1, gan.eval.run_inception)
+        d2 = gan.eval.frechet_classifier_distance(real_resized2, fake_resized2, gan.eval.run_inception)
+        return d1
+
+def buildDataset(x_path = X_PATH, y_path = Y_PATH):        
     x_Dataset = tf.data.Dataset.list_files( x_path + '/*.jpg')
     y_Dataset = tf.data.Dataset.list_files( y_path + '/*.jpg')
 
-    x_images = x_Dataset.map(lambda x: tf.image.resize_images(tf.image.decode_jpeg(tf.read_file(x), channels = INPUT_SHAPE[2]), [INPUT_SHAPE[0], INPUT_SHAPE[1]]))
-    y_images = y_Dataset.map(lambda x: tf.image.resize_images(tf.image.decode_jpeg(tf.read_file(x), channels = INPUT_SHAPE[2]), [INPUT_SHAPE[0], INPUT_SHAPE[1]]))
+    x_images = x_Dataset.map(lambda x: tf.image.resize_images(tf.image.decode_jpeg(tf.read_file(x), channels = INPUT_DIM), [INPUT_WIDTH, INPUT_WIDTH]))
+    y_images = y_Dataset.map(lambda x: tf.image.resize_images(tf.image.decode_jpeg(tf.read_file(x), channels = INPUT_DIM), [INPUT_WIDTH, INPUT_WIDTH]))
 
     xy_images = tf.data.Dataset.zip((x_images, y_images))
     xy_Dataset = xy_images.batch(BATCH_SIZE)
@@ -127,7 +127,8 @@ def load_last_checkpoint():
     saver.restore(sess, tf.train.latest_checkpoint('./'))
 
 def train():
-    for epoch in range(args.epoch):
+    for epoch in range(epochs):
+        print('========================== EPOCH %d  ==========================' % epoch)
         iterator = xy_Dataset.make_initializable_iterator()
         (x_next, y_next) = iterator.get_next()
         sess.run(iterator.initializer)
@@ -136,22 +137,20 @@ def train():
         iteration = 0
         while True:
             try:
-                X, Y = session.run([x_next, y_next])
+                X, Y = sess.run([x_next/127.5-1, y_next/127.5-1])
 
-                if x_train.shape[0] != BATCH_SIZE:
+                if X.shape[0] != BATCH_SIZE:
                     break
 
                 # Update G network and record fake outputs
                 X2Y, Y2X, _ = sess.run([model.X2Y, model.Y2X, model.g_train], feed_dict={model.X: X, model.Y: Y})
-
-                # Update D network
-                _ = sess.run(model.d_train,feed_dict={model.X: batch_imagesX, model.Y: batch_imagesY, model.X2Y_sample: X2Y, model.X2Y_sample: Y2X})
                 
-                loss_d, loss_g = sess.run([model.d_loss, model.g_loss],feed_dict={model.X: batch_imagesX, model.Y: batch_imagesY, model.X2Y_sample: X2Y, model.X2Y_sample: Y2X})
-
+                # Update D network
+                d_loss, g_loss, _ = sess.run([model.d_loss, model.g_loss, model.d_train],feed_dict={model.X: X, model.Y: Y, model.X2Y_sample: X2Y, model.Y2X_sample: Y2X})
+                
                 # Print losses
                 if iteration % log_every == 0:
-                    print('Iteration %d: Gen loss = %g | Discrim loss = %g' % (iteration, loss_g, loss_d))
+                    print('Iteration %d: Gen loss = %g | Discrim loss = %g' % (iteration, g_loss, d_loss))
                 # Save
                 if iteration % save_every == 0:
                     saver.save(sess, './dcgan_saved_model')
@@ -160,19 +159,28 @@ def train():
             except tf.errors.OutOfRangeError:
                 print('epoch ' + str( epoch) + ' end.')
                 break
+        
+        saver.save(sess, './dcgan_saved_model')
+        # sess.run(iterator.initializer)
+        # X, Y = sess.run([x_next/127.5-1, y_next/127.5-1])
+
+        # fid_ = sess.run(model.fid, feed_dict={model.X: X, model.Y: Y})
+        # print('**** INCEPTION DISTANCE: %g ****' % fid_)
 
 
 def test():
     iterator = xy_Dataset.make_initializable_iterator()
     (x_next, y_next) = iterator.get_next()
     sess.run(iterator.initializer)
-    X, Y = session.run([x_next, y_next])
+    X, Y = sess.run([x_next/127.5-1, y_next/127.5-1])
     
     gen_y, gen_x = sess.run([model.X2Y, model.Y2X], feed_dict={model.X: X, model.Y: Y})    
     
     # Rescale the image from (-1, 1) to (0, 255)
     gen_y = ((gen_y / 2) - 0.5) * 255
     gen_x = ((gen_x / 2) - 0.5) * 255
+    X = ((X / 2) - 0.5) * 255
+    Y = ((Y / 2) - 0.5) * 255
     
     # Convert to uint8
     gen_y = gen_y.astype(np.uint8)
@@ -184,29 +192,26 @@ def test():
     # Save images to disk
     for i in range(0, BATCH_SIZE):
         img_Xi = X[i]
-        s = OUT+'/X2Y_OUT/X_'+str(i)+'.png'
+        s = OUT+'/X2Y_OUT/X_'+str(i)+'.jpg'
         imsave(s, img_Xi)
         
         img_yi = gen_y[i]
-        s = OUT+'/X2Y_OUT/Y_'+str(i)+'.png'
+        s = OUT+'/X2Y_OUT/Y_'+str(i)+'.jpg'
         imsave(s, img_yi)
         
         img_Yi = Y[i]
-        s = OUT+'/Y2X_OUT/Y_'+str(i)+'.png'
+        s = OUT+'/Y2X_OUT/Y_'+str(i)+'.jpg'
         imsave(s, img_Yi)
         
         img_xi = gen_x[i]
-        s = OUT+'/Y2X_OUT/X_'+str(i)+'.png'
+        s = OUT+'/Y2X_OUT/X_'+str(i)+'.jpg'
         imsave(s, img_xi)
-
-
-# Ensure the output directory exists
-
-if RESTORE or MODE == 'test':
-    load_last_checkpoint()
+  
 
 if MODE == 'train':
     train()
+
 if MODE == 'test':
+    load_last_checkpoint()
     test()
        
